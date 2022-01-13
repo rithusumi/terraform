51592a6c3b4d96c76461bafaf13ae92a471bae80
rithusumi <77057971+rithusumi@users.noreply.github.com>
2022-01-13 12:52:56 -0600

    adding ec2 creation file

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "us-east-2"
+  shared_credentials_file = "~/.aws/credentials"
+  profile = "default"
+}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  subnet_id = "subnet-0984308a15af81821"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
