7045f10a95ba1bead1e15af1924c808c5ac470bc
rithusumi <77057971+rithusumi@users.noreply.github.com>
2022-01-14 13:45:43 -0600

    adding new branch

---
+++ s3/variable.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+default = "AKIAYSGQSMQACRPMYJRJ"
+}
+variable "aws_secret_key" {
+default = "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"
+ }
+variable "region" {
+    default = "us-east-2"
+}
