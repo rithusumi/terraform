717a1fb5c2d610187e390f9be920905aef395860
rithusumi <77057971+rithusumi@users.noreply.github.com>
2022-01-15 23:23:43 -0600

    new branch

---
+++ iam/variables.tf
@@ -0,0 +1,10 @@
+variable "username" {
+  description = "Whether to create the IAM user"
+  type        = list
+  default     = ["harshan" ,"Rithul"]
+}
+
+variable "region" {
+  default     = "us-east-2"
+}
+
